fc6929ca4f3964ce8c5edd7e192a3cc03b1980c2
Victória Mendes <vick.costa1305@gmail.com>
2023-03-14 17:01:40 -0300

    criar app core

---
+++ projeto/core/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class TimeStampedModel(models.Model):
+    created = models.DateTimeField(
+        'criado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+    modified = models.DateTimeField(
+        'modificado em',
+        auto_now_add=True,
+        auto_now=False
+    )
+
+    class Meta:
+        abstract = True
